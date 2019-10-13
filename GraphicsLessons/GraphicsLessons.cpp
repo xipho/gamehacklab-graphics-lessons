a0c28acf7466f096b2e14372aa66a6cf583298ca
Xipho <xipho@mail.ru>
2019-10-13 16:29:27 +0500

    lesson 04

--- GraphicsLessons/GraphicsLessons.cpp
+++ GraphicsLessons/GraphicsLessons.cpp
@@ -17,12 +17,19 @@ RECT clientRect;
 HBITMAP pattern;
 HBITMAP kitty;
 BLENDFUNCTION blf = { AC_SRC_OVER, 0, 255, 0 };
+bool forward = true;
+int alpha = 0;
+HDC hdc;
+bool isDrawing = true;
+HDC backDC;
+HBITMAP backBMP;
 
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+void DrawFrame();
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -106,6 +113,8 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
 
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)&DrawFrame, NULL, NULL, NULL);
+
 	return TRUE;
 }
 
@@ -115,51 +124,76 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_PAINT:
 	{
-		GetClientRect(mHwnd, &clientRect);
 		PAINTSTRUCT ps;
 		HDC hdc = BeginPaint(hWnd, &ps);
-
-		//FillRect(hdc, &clientRect, reinterpret_cast<HBRUSH>(GetStockObject(BLACK_BRUSH)));
-		HPEN newPen = CreatePen(PS_SOLID, 5, RGB(0, 0, 255));
-		HBRUSH newBrush = CreatePatternBrush(pattern); //CreateSolidBrush(RGB(255, 255, 0));
-
-		HGDIOBJ oldBrush = SelectObject(hdc, newBrush);
-		HGDIOBJ oldPen = SelectObject(hdc, newPen);
-
-		Rectangle(hdc, clientRect.left + 10, clientRect.top + 10, clientRect.right - 10, clientRect.bottom - 10);
-
-		BITMAP kittyObject;
-		GetObject(kitty, sizeof(BITMAP), &kittyObject);
-
-		HDC memDC = CreateCompatibleDC(hdc);
-		SelectObject(memDC, kitty);
-
-		//BitBlt(hdc, 30, 50, kittyObject.bmWidth, kittyObject.bmHeight, memDC, 0, 0, SRCCOPY);
-		//StretchBlt(hdc, 30, 50, kittyObject.bmWidth - 100, kittyObject.bmHeight + 25, memDC, 0, 0, 
-		//				kittyObject.bmWidth, kittyObject.bmHeight, SRCCOPY);
-
-		blf.SourceConstantAlpha = 255;
-		AlphaBlend(hdc, 20, 50, kittyObject.bmWidth, kittyObject.bmHeight, memDC, 0, 0,
-			kittyObject.bmWidth, kittyObject.bmHeight, blf);
-
-
-
-		DeleteDC(memDC);
-
-		SelectObject(hdc, oldPen);
-		SelectObject(hdc, oldBrush);
-
-		DeleteObject(newPen);
-		DeleteObject(newBrush);
-
+		BitBlt(hdc, 0, 0, clientRect.right, clientRect.bottom, backDC, 0, 0, SRCCOPY);
 		EndPaint(hWnd, &ps);
 	}
 	break;
 	case WM_DESTROY:
+		isDrawing = false;
 		PostQuitMessage(0);
 		break;
 	default:
 		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
 	return 0;
+}
+
+void DrawFrame() {
+
+	GetClientRect(mHwnd, &clientRect);
+	hdc = GetDC(mHwnd);
+	backDC = CreateCompatibleDC(hdc);
+	backBMP = CreateCompatibleBitmap(hdc, clientRect.right, clientRect.bottom);
+	HGDIOBJ oldBMP = SelectObject(backDC, backBMP);
+	ReleaseDC(mHwnd, hdc);
+
+	while (isDrawing) {
+
+		//FillRect(hdc, &clientRect, reinterpret_cast<HBRUSH>(GetStockObject(BLACK_BRUSH)));
+		HPEN newPen = CreatePen(PS_SOLID, 5, RGB(0, 0, 255));
+		HBRUSH newBrush = CreatePatternBrush(pattern); //CreateSolidBrush(RGB(255, 255, 0));
+
+		HGDIOBJ oldBrush = SelectObject(backDC, newBrush);
+		HGDIOBJ oldPen = SelectObject(backDC, newPen);
+
+		Rectangle(backDC, clientRect.left + 10, clientRect.top + 10, clientRect.right - 10, clientRect.bottom - 10);
+
+		BITMAP kittyObject;
+		GetObject(kitty, sizeof(BITMAP), &kittyObject);
+
+		HDC memDC = CreateCompatibleDC(backDC);
+		SelectObject(memDC, kitty);
+
+		//BitBlt(hdc, 30, 50, kittyObject.bmWidth, kittyObject.bmHeight, memDC, 0, 0, SRCCOPY);
+		//StretchBlt(hdc, 30, 50, kittyObject.bmWidth - 100, kittyObject.bmHeight + 25, memDC, 0, 0, 
+		//				kittyObject.bmWidth, kittyObject.bmHeight, SRCCOPY);
+
+		if (alpha >= 255) forward = false;
+		if (alpha <= 0) forward = true;
+
+		if (forward) alpha += 1;
+		else alpha -= 1;
+
+		blf.SourceConstantAlpha = alpha;
+		AlphaBlend(backDC, 20, 50, kittyObject.bmWidth, kittyObject.bmHeight, memDC, 0, 0,
+			kittyObject.bmWidth, kittyObject.bmHeight, blf);
+
+		DeleteDC(memDC);
+
+		SelectObject(backDC, oldPen);
+		SelectObject(backDC, oldBrush);
+
+		DeleteObject(newPen);
+		DeleteObject(newBrush);
+
+		RedrawWindow(mHwnd, NULL, NULL, RDW_INVALIDATE);
+
+		Sleep(1000 / 60);
+	}
+
+	SelectObject(backDC, oldBMP);
+	DeleteObject(backBMP);
+	DeleteDC(backDC);
 }
